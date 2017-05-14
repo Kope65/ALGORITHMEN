e115d12849bca5092bdf6d3ae57187147c572715
Toni Kluth <Toni Kluth>
2017-05-14 23:47:52 +0200

    final

--- src/a01/Quicksort.java
+++ src/a01/Quicksort.java
@@ -1,39 +1,72 @@
 package a01;
 
 /**
- * 
- * 
- * @author s791580, s780025
- * 
+ * @author Katharina-Sophia Bolinski s791580, Toni Kluth s780025
  */
 
 public class Quicksort {
 
+	// Unsortiertes Test-Array
 	protected static int[] intArr = { 16, 2300, 14, 15, 10, 1, -5, 200, 199,
 			201, -31, 3, 55, -157, -1250, 5, 6, 7, 8, 17, -17, 111, -111, 1111,
 			125, 145, 159, 426, 428, 456, 441, 2356, 36365, 4575, 5698, 54447,
 			451124, 1552, 45863, 25863, 15523, 565665, 2254, 5484, 121658,
 			15156, 6555 };
-	protected static String filepath = "C:/Users/Toni/Documents/PR2/ALGO/FileIntArrayMitBeispielen/Rand1000_1";
+	protected static String filepathRand = "C:/Users/Toni/Documents/PR2/ALGO/FileIntArrayMitBeispielen/Rand100_1";
+	protected static String filepathSort = "C:/Users/Toni/Documents/PR2/ALGO/FileIntArrayMitBeispielen/Sort100_1";
 	protected static long startTime;
 	protected static long endTime;
 
+	/**
+	 * Die main-Methode sortiert verschiedene Testfaelle und gibt das sortierte
+	 * Array und die benoetigte Zeit in Millisekunden aus.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// Testfall 1 mit 'intArr'
 		startTime = System.currentTimeMillis();
-		print(sort(FileIntArray.FileToIntArray(filepath)));
-		// print(sort(intArr));
+		print(sort(intArr));
+		endTime = System.currentTimeMillis();
+		System.out.println("Time: " + (endTime - startTime) + "ms");
+
+		// Testfall 2 mit 'Rand100_1'
+		startTime = System.currentTimeMillis();
+		print(sort(FileIntArray.FileToIntArray(filepathRand)));
+		endTime = System.currentTimeMillis();
+		System.out.println("Time: " + (endTime - startTime) + "ms");
+
+		// Testfall 3 mit 'Sort100_1'
+		startTime = System.currentTimeMillis();
+		print(sort(FileIntArray.FileToIntArray(filepathSort)));
 		endTime = System.currentTimeMillis();
 		System.out.println("Time: " + (endTime - startTime) + "ms");
 	}
 
+	/**
+	 * Die Methode 'sort()' stellt das uebergebene int[] fuer die Methode
+	 * 'quicksort()' bereit. Liefert ein int[] zurueck.
+	 * 
+	 * @param array
+	 * @return
+	 */
 	public static int[] sort(int[] array) {
 		quicksort(array, 0, array.length - 1);
 		return array;
 	}
 
+	/**
+	 * Die Methode 'quicksort()' teilt das uebergebene Array rekursiv in
+	 * Teil-Arrays. Liefert ein int[] zurueck.
+	 * 
+	 * @param array
+	 * @param left
+	 * @param right
+	 * @return
+	 */
 	private static int[] quicksort(int[] array, int left, int right) {
 		if (left < right) {
-			int pivot = divide(array, left, right);
+			int pivot = exchange(array, left, right);
 			if (pivot > left)
 				quicksort(array, left, pivot - 1);
 			if (pivot < right)
@@ -42,27 +75,41 @@ public class Quicksort {
 		return array;
 	}
 
-	private static int divide(int[] array, int left, int right) {
+	/**
+	 * Die Methode 'exchange()' sortiert die Teil-Arrays und fuegt sie wieder
+	 * zusammen.
+	 * 
+	 * @param array
+	 * @param left
+	 * @param right
+	 * @return
+	 */
+	private static int exchange(int[] array, int left, int right) {
+		int pivot = array[((left + right) / 2)];
 		if (array.length > 1) {
-			int pivot = array[((left + right) / 2)];
 			while (true) {
-				while (array[left] < pivot) {
+				while (array[left] < pivot) { // Suche von links Wert groeßer als 'pivot'
 					left++;
 				}
-				while (array[right] > pivot) {
+				while (array[right] > pivot) { // Suche von links Wert kleiner als 'pivot'
 					right--;
 				}
-				if (array[left] > array[right]) {
+				if (array[left] > array[right]) { // Wenn wert links kleiner als rechts, tausche Werte.
 					int helper = array[right];
 					array[right] = array[left];
 					array[left] = helper;
 				} else
-					return right;
+					return right; // Gebe rechte position zurueck
 			}
 		}
-		return left;
+		return left; // Gebe linke position zurueck
 	}
 
+	/**
+	 * Die Methode 'print()* gibt ein uebergebenes Array in der Konsole aus.
+	 * 
+	 * @param array
+	 */
 	public static void print(int[] array) {
 		System.out.println("\n" + "---sorted---");
 		for (int i = 0; i < array.length; i++) {
