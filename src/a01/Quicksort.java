24273ba9d5a6f95739daeca5cc8be410bc3c4c5a
Toni Kluth <Toni Kluth>
2017-05-14 21:06:26 +0200

    working_branch_toni push

--- src/a01/Quicksort.java
+++ src/a01/Quicksort.java
@@ -26,7 +26,7 @@ public class Quicksort {
 	private static int[] quicksort(int[] array, int left, int right) {
 		if (left < right) {
 			int pivot = divide(array, left, right);
-			if (pivot > 1)
+			if (pivot > 0)
 				quicksort(array, left, pivot - 1);
 			if (pivot + 1 < right)
 				quicksort(array, pivot + 1, right);
