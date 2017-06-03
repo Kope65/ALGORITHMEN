f57f0fe375759d22a1aa67e2cb6e5d3982e2c121
Toni Kluth <Toni Kluth>
2017-06-03 23:12:13 +0200

    get() und remove() fix

--- src/a02/Hashtable.java
+++ src/a02/Hashtable.java
@@ -2,37 +2,40 @@ package a02;
 
 import java.util.LinkedList;
 
-public class Hashtable<T> implements Map {
+public class Hashtable<T> implements Map<Object, Object> {
 
 	private LinkedList<KeyValuePair>[] hashtable;
 
 	public Hashtable(int size) {
-		hashtable = new LinkedList[size];
+		hashtable = (LinkedList<KeyValuePair>[]) new LinkedList[size];
 	}
 
 	public Object put(Object key, Object value) {
 		KeyValuePair keyValuePair = new KeyValuePair(key, value);
 		int index = key.hashCode() % hashtable.length;
 		System.out.println(hashtable[index]);
-		if (hashtable[index].equals(null)) {
-			hashtable[index].add(0, keyValuePair);
-		} else
+		if (hashtable[index].equals(null)) {			
+			LinkedList<KeyValuePair> list = new LinkedList<KeyValuePair>();
+			list.add(keyValuePair);
+			hashtable[index] = list;
+		} else {
 			for (int i = 0; i < hashtable[index].size(); i++) {
 				if (hashtable[index].get(i).getKey().equals(key)) {
 					Object returnValue = hashtable[index].get(i).getValue();
 					hashtable[index].get(i).setValue(value);
 					return returnValue;
 				} else {
-					hashtable[index].add(i, keyValuePair);
+					hashtable[index].add(keyValuePair);
 				}
 			}
+		}
 		return null;
 	}
 
 	public Object get(Object key) {
 		int index = key.hashCode() % hashtable.length;
-		for (int i = 0; i < hashtable[index].size(); i++) {			
-			if (hashtable[index].get(i).equals(key)) {
+		for (int i = 0; i < hashtable[index].size(); i++) {
+			if (hashtable[index].get(i).getKey().equals(key)) {
 				Object returnValue = hashtable[index].get(i).getValue();
 				return returnValue;
 			}
@@ -42,11 +45,10 @@ public class Hashtable<T> implements Map {
 
 	public Object remove(Object key) {
 		int index = key.hashCode() % hashtable.length;
-		for (int i = 0; i < hashtable[index].size(); i++) {			
-			if (hashtable[index].get(i).equals(key)) {
+		for (int i = 0; i < hashtable[index].size(); i++) {
+			if (hashtable[index].get(i).getKey().equals(key)) {
 				Object returnValue = hashtable[index].get(i).getValue();
-				hashtable[index].get(i).setKey(null);
-				hashtable[index].get(i).setValue(null);
+				hashtable[index].remove();
 				return returnValue;
 			}
 		}
@@ -70,10 +72,10 @@ public class Hashtable<T> implements Map {
 			return this.value;
 		}
 
-		protected void setKey(Object key) {
-			this.key = key;
-		}
-		
+		// protected void setKey(Object key) {
+		// this.key = key;
+		// }
+
 		protected void setValue(Object value) {
 			this.value = value;
 		}
