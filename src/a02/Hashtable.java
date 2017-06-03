b1b0e763d2f582f0d14ea16138f025e8ff7cf07d
Toni Kluth <Toni Kluth>
2017-06-03 20:52:09 +0200

    update Samstag

--- src/a02/Hashtable.java
+++ src/a02/Hashtable.java
@@ -4,39 +4,85 @@ import java.util.LinkedList;
 
 public class Hashtable<T> implements Map {
 
-	private static Object[] hashtable;
+	private LinkedList<KeyValuePair>[] hashtable;
 
 	public Hashtable(int size) {
-		hashtable = new Object[size];
+		hashtable = new LinkedList[size];
 	}
 
 	public Object put(Object key, Object value) {
-		int index = key.hashCode() % hashtable.length;
-		Object returnvalue = hashtable[index];
 		KeyValuePair keyValuePair = new KeyValuePair(key, value);
-		hashtable[index] = keyValuePair; 
-		return returnvalue;
+		int index = key.hashCode() % hashtable.length;
+		System.out.println(hashtable[index]);
+		if (hashtable[index].equals(null)) {
+			hashtable[index].add(0, keyValuePair);
+		} else
+			for (int i = 0; i < hashtable[index].size(); i++) {
+				if (hashtable[index].get(i).getKey().equals(key)) {
+					Object returnValue = hashtable[index].get(i).getValue();
+					hashtable[index].get(i).setValue(value);
+					return returnValue;
+				} else {
+					hashtable[index].add(i, keyValuePair);
+				}
+			}
+		return null;
 	}
 
 	public Object get(Object key) {
 		int index = key.hashCode() % hashtable.length;
-		return hashtable[index];
+		for (int i = 0; i < hashtable[index].size(); i++) {			
+			if (hashtable[index].get(i).equals(key)) {
+				Object returnValue = hashtable[index].get(i).getValue();
+				return returnValue;
+			}
+		}
+		return null;
 	}
 
 	public Object remove(Object key) {
 		int index = key.hashCode() % hashtable.length;
-		Object returnvalue = hashtable[index];
-		hashtable[index] = null;
-		return returnvalue;
-	}
-
-	protected class KeyValuePair {
-		private LinkedList<Object> list;
-
-		public KeyValuePair(Object key, Object value) {
-			list.add(key);
-			list.add(value);
+		for (int i = 0; i < hashtable[index].size(); i++) {			
+			if (hashtable[index].get(i).equals(key)) {
+				Object returnValue = hashtable[index].get(i).getValue();
+				hashtable[index].get(i).setKey(null);
+				hashtable[index].get(i).setValue(null);
+				return returnValue;
+			}
 		}
+		return null;
 	}
+
+	private class KeyValuePair {
+		Object key;
+		Object value;
+
+		protected KeyValuePair(Object key, Object value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		protected Object getKey() {
+			return this.key;
+		}
+
+		protected Object getValue() {
+			return this.value;
+		}
+
+		protected void setKey(Object key) {
+			this.key = key;
+		}
+		
+		protected void setValue(Object value) {
+			this.value = value;
+		}
+
+		@Override
+		public String toString() {
+			return "KeyValuePair [key=" + key + ", value=" + value + "]";
+		}
+
+	} // end of inner class KeyValuePair
 
 }
--- src/a02/HashtableTest.java
+++ src/a02/HashtableTest.java
@@ -3,11 +3,8 @@ package a02;
 public class HashtableTest {
 
 	public static void main(String[] args) {
-		Hashtable<String> hashtable = new Hashtable<String>(10);
+		Hashtable<Object> hashtable = new Hashtable<Object>(10);
 		hashtable.put("Hallo", "Welt");
-		System.out.println(hashtable.get("Hallo"));
-		hashtable.remove("Hallo");
-		System.out.println(hashtable.get("Hallo"));
 	}
 
 }
