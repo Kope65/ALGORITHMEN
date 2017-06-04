9037c33de157ccb02ef03fc9cd777a0e9d55ddb1
MissRina <rina@missrina.de>
2017-06-04 19:23:39 +0200

    Kommentare zur Zusatzaufgabe

--- src/a02/Hashtable.java
+++ src/a02/Hashtable.java
@@ -1,115 +1,242 @@
 package a02;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
-public class Hashtable<T> implements Map {
 
+/**
+ * @author Katharina-Sophia Bolinski s791580, Toni Kluth s780025
+ *
+ * @param <T> Generischer Typ der Klasse 'Hashtable'
+ */
+public class Hashtable<T> implements Map {
 	private LinkedList<KeyValuePair>[] hashtable;
 
+	/**
+	 * Konstruktor der Klasse 'Hashtable'. Initialisiert ein 'LinkedList<KeyValuePair>[]'
+	 * und fuellt alle Indices mit leeren 'LinkedLists'.
+	 * 
+	 * @param size Die groesse des zu erstellenden 'LinkedList<KeyValuePair>[]'
+	 */
 	public Hashtable(int size) {
-		hashtable = (LinkedList<KeyValuePair>[]) new LinkedList[size];
+		hashtable = new LinkedList[size];
 		for (int i = 0; i < hashtable.length; i++) {
 			hashtable[i] = new LinkedList<KeyValuePair>();
 		}
 	}
-
 	
 	/**
-	 * Methode zum Einfügen des KeyValue-Paares
-	 * @param key
-	 * @param value
-	 * @return falls es schon einen Wert mit Schluessel key in der Map 
-	 *         gab, wird der alte Wert entfernt und zurueckgegeben; 
-	 *         sonst wird null zurueckgegeben
-	 *  **/
-	
+	 * Methode zum Einfuegen des Schluessel-Wert-Paars.
+	 * 
+	 * @param key 	Der Schluessel des Schluessel-Wert-Paar das in das 'LinkedList<KeyValuePair>[]'
+	 * 				eingefuegt werden soll
+	 * @param value Der Wert des Schluessel-Wert-Paar das in das 'LinkedList<KeyValuePair>[]'
+	 * 				eingefuegt werden soll
+	 * @return Object: falls es schon einen Wert mit Schluessel 'key' in der Map 
+	 *         gibt, wird der alte Wert entfernt und zurueckgegeben; 
+	 *         sonst wird 'null' zurueckgegeben
+	 **/
 	public Object put(Object key, Object value) {
-		KeyValuePair keyValuePair = new KeyValuePair(key, value);
-		int index = key.hashCode() % hashtable.length; 						// Formel zur Berechnung
-		if (hashtable[index].isEmpty()) {									//wenn leer, dann...
-			LinkedList<KeyValuePair> list = new LinkedList<KeyValuePair>(); //...neue LinkedList und...
-			list.add(keyValuePair); 										//..füge keyValuePair zur LinkedList hinzu
-			hashtable[index] = list;
-		} else { 															//sonst,...
+		hashtable = checkAndResizeHashtable(hashtable);							/***** ZUSATZAUFGABE *****/
+		KeyValuePair keyValuePair = new KeyValuePair(key, value);				// Erstelle ein 'KeyValuePair' aus den uebergebenen Werten.
+		double indexDouble = Math.ceil(key.hashCode() % hashtable.length); 		// Ermittle den Index zum 'key' als 'double'.
+		int index = (int) Math.ceil(indexDouble);								// Runde den 'double' auf und mache cast zum 'int'
+		if(index < 0) {															// Wenn 'index' negativ...
+			index *= -1;														// ... multipliziere 'index' mit -1.
+		}
+		System.out.println("Index: " + index);									/** Ausgabe für Test **/
+		System.out.println("BEFORE put(): " + hashtable[index]);				/** Ausgabe für Test **/
+		if (hashtable[index].isEmpty()) {										// Wenn die Liste an Index 'index' leer ist...
+			hashtable[index].add(keyValuePair);									// ... fuege 'KeyValuePair' an der Stelle ein.
+		} else { 															
 			for (int i = 0; i < hashtable[index].size(); i++) {
-				if (hashtable[index].get(i).getKey().equals(key)) { 		//...vergleiche keys
-					Object returnValue = hashtable[index].get(i).getValue();
-					hashtable[index].get(i).setValue(value);
-					return returnValue; 									// und gebe Value(Wert) aus
+				if (hashtable[index].get(i).getKey().equals(key)) { 			// Vergleiche Keys.
+					Object returnValue = hashtable[index].get(i).getValue();	// Packe alten 'value' in Hilfsfeld.
+					hashtable[index].get(i).setValue(value);					// Ueberschreibe 'value'.
+					System.out.println("AFTER put(): " + hashtable[index]);		/** Ausgabe für Test **/
+					return returnValue; 										// Gebe den alten 'value' zurueck.
 				} else {
-					hashtable[index].add(keyValuePair);
+					hashtable[index].add(keyValuePair);							// Wenn 'key' nicht vorhanden ist, fuege 'KeyValuePair' hinzu.
+				}																
+			}																	
+		}
+		System.out.println("AFTER put(): " + hashtable[index]);					/** Ausgabe für Test **/
+		return null;
+	}
+	
+	/**
+	 * Methode zur Bestimmung des 'value' zu einem 'key' in Map
+	 * 
+	 * @param key Der Schluessel des gesuchten 'value'.
+	 * @return Object: Gibt den Wert zum Schluessel 'key' zurueck, falls ein 
+	 *         entsprechendes Schluessel-Wert-Paar vorhanden ist; 
+	 *         sonst 'null'
+	 **/
+	public Object get(Object key) {
+		double indexDouble = Math.ceil(key.hashCode() % hashtable.length); 		// Ermittle den Index zum 'key'.
+		int index = (int) Math.ceil(indexDouble);								// Runde den 'double' auf und mache cast zum 'int'
+		if(index < 0) {															// Wenn 'index' negativ...
+			index *= -1;														// ... multipliziere 'index' mit -1.
+		}
+		if (!hashtable[index].isEmpty()) {										
+			for (int i = 0; i < hashtable[index].size(); i++) {	
+				if (hashtable[index].get(i).getKey().equals(key)) {				// Vergleiche Keys.
+					Object returnValue = hashtable[index].get(i).getValue();	// Packe alten 'value' in Hilfsfeld.
+					return returnValue;											// Gebe den alten 'value' zurueck.
 				}
 			}
 		}
-		return null;
-	}
-
-	
-	/**
-	 * Methode zur Bestimmung des Values zu einem Key in Map
-	 * @param key
-	 * @return den Wert zum Schluessel key, falls ein 
-	 *         entsprechendes Schluessel-Wert-Paar vorhanden ist; 
-	 *         null sonst
-	 * **/
-	
-	public Object get(Object key) {
-		int index = key.hashCode() % hashtable.length;						// Formel zur Berechnung
-		for (int i = 0; i < hashtable[index].size(); i++) {
-			if (hashtable[index].get(i).getKey().equals(key)) {				// zum vergleichen wir equals verwendet
-				Object returnValue = hashtable[index].get(i).getValue();	//
-				return returnValue;											// gebe Value(Wert) aus
-			}
-		}
-		return null;
+		return null;															// Gibt 'null' zurueck, wenn 'key' nicht vorhanden ist.
 	}
 
 	/**
-	 * Methode zum Löschen KeyValue-Paares aus der Map 
-	 * @param key der Schluessel 
-	 * @return der Wert zu dem Schluessel, falls ein entsprechendes 
+	 * Methode zum Loeschen Schluessel-Wert-Paar aus der Map 
+	 * 
+	 * @param key der Schluessel des zu loeschenden 'value'
+	 * @return Object: der Wert zu dem Schluessel, falls ein entsprechendes 
 	 *         Schluessel-Wert-Paar vorhanden ist; 
-	 *         null sonst
-	 *  **/
-	
+	 *         sonst 'null'
+	 **/
 	public Object remove(Object key) {
-		int index = key.hashCode() % hashtable.length;						// Formel zur Berechnung
-		for (int i = 0; i < hashtable[index].size(); i++) {
-			if (hashtable[index].get(i).getKey().equals(key)) { 			// zum vergleichen wir equals verwendet
-				Object returnValue = hashtable[index].get(i).getValue();	//
-				hashtable[index].remove();									// lösche Eintrag
-				return returnValue;
+		double indexDouble = Math.ceil(key.hashCode() % hashtable.length); 		// Ermittle den Index zum 'key' als 'double'.
+		int index = (int) Math.ceil(indexDouble);								// Runde den 'double' auf und mache cast zum 'int'
+		if(index < 0) {															// Wenn 'index' negativ...
+			index *= -1;														// ... multipliziere 'index' mit -1.
+		}
+		if (!hashtable[index].isEmpty()) {										
+			for (int i = 0; i < hashtable[index].size(); i++) {
+				if (hashtable[index].get(i).getKey().equals(key)) { 			// Vergleiche Keys.
+					Object returnValue = hashtable[index].get(i).getValue();	// Packe alten 'value' in Hilfsfeld.
+					hashtable[index].remove();									// loesche Eintrag
+					return returnValue;											// Gebe den alten 'value' zurueck.
+				}
 			}
 		}
-		return null;
+		return null;															// Gibt 'null' zurueck, wenn 'key' nicht vorhanden ist.
+	}
+	
+	// ***** GETTER ZUM TESTEN **********************
+	/**
+	 * Methode zum pruefen der Groesse des 'LinkedList<KeyValuePair>[]'.
+	 * 
+	 * @return int: die Groesse des 'LinkedList<KeyValuePair>[]'.
+	 */
+	public int getHashtableSize() {
+		return hashtable.length;
+	}
+	
+	/**
+	 * Methode zum pruefen der Anzahl der Elemente in der 'LinkedList<KeyValuePair>[]'.
+	 * 
+	 * @return int: die Anzahl der Elemente der 'LinkedList<KeyValuePair>[]'.
+	 */
+	public int getElementCount() { 												// Rufe Zaehler auf
+		int counter = 0;
+		for (int i = 0; i < hashtable.length; i++) {
+			if(!hashtable[i].isEmpty()) {
+				for (int j = 0; j < hashtable[i].size(); j++) {
+					if(!hashtable[i].get(j).equals(null))
+						counter++;												// Wenn Element vorhanden, zaehle 'counter' hoch.
+				}
+			}																			
+		}
+		return counter; 														// Gebe Zaehler aus
+	}
+	
+	// ***** ZUSATZAUFGABE **********************	
+	/**
+	 * Methode prueft das Verhaeltnis von Elementen zu der 
+	 * Groesse des ubergebenen 'LinkedList<KeyValuePair>[]'.
+	 * Wenn das Verhaltnis > 2 ist, Erstelle ein neues 
+	 * 'LinkedList<KeyValuePair>[]' und gebe es zurueck.
+	 * Ansonsten gebe das ubergebene 'LinkedList<KeyValuePair>[]'
+	 * unveraendert zurueck.
+	 * 
+	 * @param hashtableToCheck Das zu pruefende 'LinkedList<KeyValuePair>[]'
+	 * @return LinkedList<KeyValuePair>[]: Gibt ein 'LinkedList<KeyValuePair>[]'.
+	 */
+	public LinkedList<KeyValuePair>[] checkAndResizeHashtable(LinkedList<KeyValuePair>[] hashtableToCheck) { //ueberpruefe und vergroessere Hashtable
+		if((hashtableToCheck.length/2) <= getElementCount()) {												 
+			LinkedList<KeyValuePair>[] helperHashtable = new LinkedList[hashtableToCheck.length * 2];		 //fuegt KeyValuePair in Hilfstabelle ein
+			for (int i = 0; i < helperHashtable.length; i++) {												 
+				helperHashtable[i] = new LinkedList<KeyValuePair>(); 										 
+			}
+			for (int i = 0; i < hashtableToCheck.length; i++) {
+				if(!hashtableToCheck[i].isEmpty()) {														 //falls i leer...
+					for (int j = 0; j < hashtableToCheck[i].size(); j++) {									 
+						if(!hashtableToCheck[i].get(j).equals(null)) {										 //...vergleiche Eintraege
+							double helperIndexDouble = Math.ceil(hashtableToCheck[i].get(j).getKey().hashCode() % helperHashtable.length); //ermittle den helperIndex zum 'key'	als 'double'	
+							int helperIndex = (int) Math.ceil(helperIndexDouble);							 //Runde den 'double' auf
+							if(helperIndex < 0) {
+								helperIndex *= -1;															 //wenn negativ, multipliziere 'helperIndex' mit -1
+							}
+							KeyValuePair helperKeyValuePair = new KeyValuePair(hashtableToCheck[i].get(j).getKey(), hashtableToCheck[i].get(j).getValue());
+							helperHashtable[helperIndex].add(helperKeyValuePair);
+						}
+					}
+				}																			
+			}
+			return helperHashtable; // gibt Eintrag aus 'helperHashtable' aus
+		}
+		return hashtableToCheck; // gibt Eintrag aus 'hashtableToCheck' au
 	}
 
-	private class KeyValuePair {
+	@Override
+	public String toString() {
+		return "Hashtable [hashtable = " + Arrays.toString(hashtable) + "]";
+	}
+
+	
+	private class KeyValuePair {		
 		Object key;
 		Object value;
 
+		/**
+		 * Der Konstruktor der inneren Klasse 'KeyValuePair'
+		 * Hier wird ein Schluessel-Wert-Paar mit den uebergebenen 
+		 * Parametern erstellt. 
+		 * 
+		 * @param key Der Schluessel des 'KeyValuePair'
+		 * @param value Der Wert des 'KeyValuePair'
+		 */
 		protected KeyValuePair(Object key, Object value) {
 			this.key = key;
 			this.value = value;
 		}
 
+		/**
+		 * Getter-Methode fuer den Schluessel.
+		 * 
+		 * @return Object: Gibt 'key' zurueck 
+		 */
 		protected Object getKey() {
 			return this.key;
 		}
 
+		/**
+		 * Getter-Methode fuer den Wert.
+		 * 
+		 * @return Object: Gibt 'value' zurueck 
+		 */
 		protected Object getValue() {
 			return this.value;
 		}
-
+		
+		/**
+		 * Setter-Methode fuer den Wert.
+		 * 
+		 * @param value Der Wert der im Schluessel-Wert-Paar
+		 * 				geaendert werden soll
+		 */
 		protected void setValue(Object value) {
 			this.value = value;
 		}
 
 		@Override
 		public String toString() {
-			return "KeyValuePair [key=" + key + ", value=" + value + "]";
+			return "KeyValuePair [key = " + key + ", value = " + value + "]";
 		}
 
-	} // end of inner class KeyValuePair
+	} // Ende der inneren Klasse 'KeyValuePair'.
 
 }
--- src/a02/HashtableTest.java
+++ src/a02/HashtableTest.java
@@ -1,30 +1,181 @@
 package a02;
 
+import java.util.Vector;
+
 public class HashtableTest {
 
 	public static void main(String[] args) {
-		Hashtable<Object> hashtable = new Hashtable<Object>(10);
+		Hashtable<Object> hashtable = new Hashtable<Object>(7);
+		float floatKey = 16.345f;
+		float floatValue = 20.25698f;
+		
 		hashtable.put("Hallo", "Welt");
-		System.out.println(hashtable.get("Hallo"));
-		hashtable.put("Hallo", "Weltraum");		
-		System.out.println(hashtable.get("Hallo"));
+		System.out.println("Value: " + hashtable.get("Hallo"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Servus", "Planet");
+		System.out.println("Value: " + hashtable.get("Servus"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Hallo", "Weltraum");														// UEBERSCHREIBE EINTRAG
+		System.out.println("Value: " + hashtable.get("Hallo") + " UEBERSCHRIEBEN");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
 		hashtable.put("6", "12");
-		System.out.println(hashtable.get("6"));
-		hashtable.remove("Hallo");
+		System.out.println("Value: " + hashtable.get("6"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("6", 15);																	// UEBERSCHREIBE EINTRAG
+		System.out.println("Value: " + hashtable.get("6") + " UEBERSCHRIEBEN");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put(6, 50);																	
+		System.out.println("Value: " + hashtable.get(6));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.remove("Hallo"); 																// ENTFERNE EINTRAG
+		System.out.println("Value: " + hashtable.get("Hallo")   + " ENTFERNT");
 		System.out.println(hashtable.get("Hallo"));
-		System.out.println(hashtable.get("6"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
 		hashtable.put("Cat", "Dog");
-		System.out.println(hashtable.get("Cat"));
-		hashtable.put("Cat", "Hotdog");
-		System.out.println(hashtable.get("Cat"));
+		System.out.println("Value: " + hashtable.get("Cat"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Cat", "Hotdog"); 														// UEBERSCHREIBE EINTRAG
+		System.out.println("Value: " + hashtable.get("Cat") + " UEBERSCHRIEBEN");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
 		hashtable.put("Zahl", "15");
 		System.out.println(hashtable.get("Zahl"));
-		hashtable.remove("Cat");
-		System.out.println(hashtable.get("Cat"));
-		System.out.println(hashtable.get("Zahl"));
-	
-	
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
 		
+		hashtable.remove("Cat"); 																// ENTFERNE EINTRAG
+		System.out.println("Value: " + hashtable.get("Cat"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
 		
+		hashtable.put(floatKey, floatValue);		
+		System.out.println(hashtable.get(floatKey));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.remove(floatKey); 																// ENTFERNE EINTRAG
+		System.out.println("Value: " + hashtable.get(floatKey) + " ENTFERNT");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Wie", "So");
+		System.out.println("Value: " + hashtable.get("Wie"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Wo", "Da");
+		System.out.println(hashtable.get("Wo"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Moin", "Moin moin");
+		System.out.println("Value: " + hashtable.get("Moin"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Tach", "Tschuess");
+		System.out.println("Value: " + hashtable.get("Tach"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put(0, 11);
+		System.out.println(hashtable.get(0));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.remove(0); 																// ENTFERNE EINTRAG
+		System.out.println("Value: " + hashtable.get(0)   + " ENTFERNT");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Elefant", "Gedaechtnis");
+		System.out.println("Value: " + hashtable.get("Elefant"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Elefant", "Gedaechtnisluecke");										// UEBERSCHREIBE EINTRAG
+		System.out.println("Value: " + hashtable.get("Elefant") + " UEBERSCHRIEBEN");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put("Hola", "Adios");
+		System.out.println("Value: " + hashtable.get("Hola"));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.put(30, 12.345678f);
+		System.out.println("Value: " + hashtable.get(30));
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
+		
+		hashtable.remove(30); 																// ENTFERNE EINTRAG
+		System.out.println("Value: " + hashtable.get(30) + " ENTFERNT");
+		System.out.println(hashtable.toString());
+		System.out.println("Anzahl der Eintraege im Hashtable: " + hashtable.getElementCount());
+		System.out.println("Groesse des Hashtable: " + hashtable.getHashtableSize());
+		System.out.println("*****");
 	}
 }
